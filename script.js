72ceb36ed9faf247cc98e0cfd0eed6793ff69322
shhh7612 <yogscast.sebastian@gmail.com>
2021-08-17 22:16:16 -0500

    Style Update

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
+const maxWidth = 1056
+
 function dropdown(id) {
-    if(!document.getElementById("dd" + id).classList.contains('show')){
-        Array.from(document.getElementsByClassName("show")).forEach(drop => {drop.classList.remove('show')});
-        Array.from(document.getElementsByClassName("droprotate")).forEach(drop => {drop.classList.remove('droprotate')});
+    if(!document.getElementById('dd' + id).classList.contains('showdropdown')){
+        Array.from(document.getElementsByClassName('showdropdown')).forEach(drop => {drop.classList.remove('showdropdown')});
+        Array.from(document.getElementsByClassName('droprotate')).forEach(drop => {drop.classList.remove('droprotate')});
     }
-    document.getElementById("dd" + id).classList.toggle("show");
-    document.getElementById("da" + id).classList.toggle("droprotate");
+    document.getElementById('dd' + id).classList.toggle('showdropdown');
+    document.getElementById('da' + id).classList.toggle('droprotate');
+    console.log(id)
 }
 
 window.onclick = function(e) {
-    if (!e.target.matches('.dropbtn')) {
-        Array.from(document.getElementsByClassName("show")).forEach(drop => {drop.classList.remove('show')});
-        Array.from(document.getElementsByClassName("droprotate")).forEach(drop => {drop.classList.remove('droprotate')});
+    if (window.screen.width >= maxWidth) {
+        if (!e.target.matches('.dropbtn')) {
+            Array.from(document.getElementsByClassName('showdropdown')).forEach(drop => {drop.classList.remove('showdropdown')});
+            Array.from(document.getElementsByClassName('droprotate')).forEach(drop => {drop.classList.remove('droprotate')});
+            console.log(`closed ${e}`)
+        }
+        console.log(e)
     }
 }
-
+function toggleNav() {
+    document.getElementById('nav').classList.toggle('extend');
+    document.getElementById('navticon').classList.toggle('fa-bars');
+    document.getElementById('navticon').classList.toggle('fa-times');
+    Array.from(document.getElementsByClassName('showdropdown')).forEach(drop => {drop.classList.remove('showdropdown')});
+    Array.from(document.getElementsByClassName('droprotate')).forEach(drop => {drop.classList.remove('droprotate')});
+}
 function copyall(block) {
-    let text = document.getElementById(block).children[1].innerText;
-    navigator.clipboard.writeText(text).then(function() {
-        console.log('Async: Copying to clipboard was successful!');
-      }, function(err) {
-        console.error('Async: Could not copy text: ', err);
-      });
-  }
+    let element = document.getElementById(block);
+    for (let i = 0; i < element.children.length; i++) {
+        if(element.children[i].tagName === 'PRE'){
+            navigator.clipboard.writeText(element.children[i].innerText).then(function() {
+                console.log('Async: Copying to clipboard was successful!', element.children[i].innerText);
+              }, function(err) {
+                console.error('Async: Could not copy text: ', err);
+            });
+        }
+    }
+}
+window.addEventListener('resize', ()=>{
+    if (window.screen.width >= maxWidth) {
+        if(document.getElementById('nav').classList.contains('extend')){
+            toggleNav()
+        }
+    }
+})
