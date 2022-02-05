166ce7f17f2f63e7dd1150c50ef955d8820d8568
shhh7612 <yogscast.sebastian@gmail.com>
2022-02-04 21:56:14 -0600

    Beta

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
 redirect();
 
+class anchor_custom extends HTMLElement {
+    constructor() {
+        super();
+        this.href = this.getAttribute('href');
+        this.target = this.getAttribute('target');
+        this.setAttribute('tabindex', 0);
+        if (this.target === null) this.target = '_self';
+        this.addEventListener('click', e => {
+            if (this.href) window.open(this.href, this.target, '');
+        });
+    }
+}
+
+class details_custom extends HTMLElement {
+    constructor() {
+        super();
+        this.setAttribute('tabindex', 0);
+        this.addEventListener('click', e => {
+            this.toggleAttribute('open');
+        });
+    }
+}
+
+window.customElements.define("nav-item", anchor_custom);
+window.customElements.define("nav-dropdown", details_custom);
+
 function openNav() {
-    let x = document.getElementById("navigation");
+    let x = document.getElementsByTagName('nav').item(0)
+    console.log(x)
     let y = document.getElementById("main");
     if (x.className === "normal") {
         x.className += " responsive";
@@ -22,9 +49,13 @@ const debounce = (fn) => {
     }
 };
 const storeScroll = () => {
+    let limit = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
     document.documentElement.dataset.scroll = window.scrollY;
+    document.body.style.setProperty('--scroll', `${-100 + ((window.scrollY / (limit - window.innerHeight)) * 100)}px`)
 }
-document.addEventListener('scroll', debounce(storeScroll), { passive: true });
+document.addEventListener('scroll', debounce(storeScroll), {
+    passive: true
+});
 storeScroll();
 
 function createPopup(title, text, buttonText, buttonData) {
@@ -57,17 +88,39 @@ if (localStorage.getItem('cookies') === null) {
         ['acceptCookies(this.parentElement.parentElement.parentElement)', 'declineCookies(this.parentElement.parentElement.parentElement)']
     );
 } else {
-    console.log(`its ${localStorage.getItem('cookies')}`)
+
+}
+if (localStorage.getItem('is16+') === null || localStorage.getItem('is16+') === 'false') {
+    createPopup(
+        'Are you 16 or older?',
+        "This website contains some things that some users might find disturbing, so we require you to be 16 or older.",
+        ['Yes', 'No'],
+        ['is16(this.parentElement.parentElement.parentElement)', 'not16(this.parentElement)']
+    );
+} else {
+
 }
 
 function acceptCookies(popup) {
     popup.remove();
     localStorage.setItem('cookies', true);
 }
+
 function declineCookies(popup) {
     popup.remove();
     localStorage.setItem('cookies', false);
 }
+
+function is16(popup) {
+    popup.remove();
+    localStorage.setItem('is16+', true);
+}
+
+function not16(parent) {
+    localStorage.setItem('is16+', false);
+    parent.innerHTML = 'Thank you for your honesty. Your reward is <a style="color:var(--themeColor);"href="https://www.youtube.com/watch?v=1CnYiuKRdlI">this video</a> of a frog in a top hat.'
+}
+
 
 function copyall(block) {
     let element = document.getElementById(block);
@@ -80,53 +133,66 @@ function copyall(block) {
     }
 }
 
-function fetchStats() {
-    if (location.toString().includes('/aika')) {
-        fetch(atob(`aHR0cHM6Ly90b3AuZ2cvYXBpL2JvdHMvODQ3MDMyNTY4Nzk2MTUxODI4`), { method: `GET`, headers: { "Authorization": [`eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9`, `eyJpZCI6Ijg0NzAzMjU2ODc5NjE1MTgyOCIsImJvdCI6dHJ1ZSwiaWF0IjoxNjI0MTI0Mjg3fQ`, `fdzMuwXwuAd03lLBlZ42KVCsxcM6D4a2ee4gm7v1uqk`].join(`.`) } }).then(data => { data.json().then(data => { document.getElementsByClassName(atob(`Ym90LXNlcnZlci1jb3VudA==`))[0].innerText = data[atob(`c2VydmVyX2NvdW50`)] }) })
-    }
-    if (document.getElementById(`contributors`)) {
-        let downloads = 0;
-        fetch(`https://api.npmjs.org/downloads/range/2013-08-21:2100-08-21/ards-client`).then(data => {
-            data.json().then(data => {
-                data.downloads.forEach(day => downloads = downloads + day.downloads);
-                document.getElementById(`downloads`).innerText = downloads.toLocaleString() + ` downloads`;
-            })
-        })
-        fetch(`https://api.github.com/repos/shhh7612/ards-client`).then(data => {
-            data.json().then(data => {
-                document.getElementById(`stars`).innerText = data.stargazers_count.toLocaleString() + ` stars`;
-                document.getElementById(`forks`).innerText = data.forks_count.toLocaleString() + ` forks`;
-                document.getElementById(`openIssues`).innerText = data.open_issues_count.toLocaleString() + ` open issues`
-            })
-        })
-        fetch(`https://api.github.com/repos/shhh7612/ards-client/stats/contributors`).then(data => {
-            data.json().then(data => {
-                document.getElementById(`contributors`).innerText = data.length.toLocaleString() + ` contributors`;
-            })
-        })
-    }
-}
 function isUpper(str) {
     return !/[a-z]/.test(str) && /[A-Z]/.test(str);
 }
+
 function redirect() {
-    if (isUpper(location.toString())) { location.replace(location.toString().toLowerCase()) }
-    if (location.toString() === `${location.origin}/Ards-Client/classes/hentai`) { location.replace(`${location.origin}/ards-client/docs/v2/classes/hentai`) }
-    if (location.toString() === `${location.origin}/Ards-Client/classes/hentai/danbooru`) { location.replace(`${location.origin}/ards-client/docs/v2/classes/hentai/danbooru`) }
-    if (location.toString() === `${location.origin}/Ards-Client/classes/hentai/konachan`) { location.replace(`${location.origin}/ards-client/docs/v2/classes/hentai/konachan`) }
-    if (location.toString() === `${location.origin}/Ards-Client/classes/hentai/neko`) { location.replace(`${location.origin}/ards-client/docs/v2/classes/hentai/neko`) }
-    if (location.toString() === `${location.origin}/Ards-Client/classes/hentai/rule34`) { location.replace(`${location.origin}/ards-client/docs/v2/classes/hentai/rule34`) }
-    if (location.toString() === `${location.origin}/Ards-Client/classes/hentai/yandere`) { location.replace(`${location.origin}/ards-client/docs/v2/classes/hentai/yandere`) }
-    if (location.toString() === `${location.origin}/Ards-Client/classes/meme`) { location.replace(`${location.origin}/ards-client/docs/v2/classes/meme`) }
-    if (location.toString() === `${location.origin}/Ards-Client/classes/meme/random`) { location.replace(`${location.origin}/ards-client/docs/v2/classes/meme/random`) }
-    if (location.toString() === `${location.origin}/Ards-Client/classes/porn`) { location.replace(`${location.origin}/ards-client/docs/v2/classes/porn`) }
-    if (location.toString() === `${location.origin}/Ards-Client/classes/porn/ass`) { location.replace(`${location.origin}/ards-client/docs/v2/classes/porn/ass`) }
-    if (location.toString() === `${location.origin}/Ards-Client/classes/porn/boobs`) { location.replace(`${location.origin}/ards-client/docs/v2/classes/porn/boobs`) }
-    if (location.toString() === `${location.origin}/Ards-Client/classes/porn/panties`) { location.replace(`${location.origin}/ards-client/docs/v2/classes/porn/panties`) }
-    if (location.toString() === `${location.origin}/Ards-Client/classes/porn/pussy`) { location.replace(`${location.origin}/ards-client/docs/v2/classes/porn/pussy`) }
-    if (location.toString() === `${location.origin}/Ards-Client/classes/porn/random`) { location.replace(`${location.origin}/ards-client/docs/v2/classes/porn/random`) }
-    if (location.toString() === `${location.origin}/Ards-Client/classes/porn/thighs`) { location.replace(`${location.origin}/ards-client/docs/v2/classes/porn/thighs`) }
-    if (location.toString() === `${location.origin}/Ards-Client/classes/reddit`) { location.replace(`${location.origin}/ards-client/docs/v2/classes/reddit`) }
-    if (location.toString() === `${location.origin}/Ards-Client/classes/reddit/custom`) { location.replace(`${location.origin}/ards-client/docs/v2/classes/reddit/custom`) }
-    if (location.toString() === `${location.origin}/Ards-Client/classes/reddit/custom_1`) { location.replace(`${location.origin}/ards-client/docs/v2/classes/reddit/custom_1`) }
+    if (isUpper(location.toString())) {
+        location.replace(location.toString().toLowerCase())
+    }
+    if (location.toString() === `${location.origin}/Ards-Client/classes/hentai`) {
+        location.replace(`${location.origin}/ards-client/docs/v2/classes/hentai`)
+    }
+    if (location.toString() === `${location.origin}/Ards-Client/classes/hentai/danbooru`) {
+        location.replace(`${location.origin}/ards-client/docs/v2/classes/hentai/danbooru`)
+    }
+    if (location.toString() === `${location.origin}/Ards-Client/classes/hentai/konachan`) {
+        location.replace(`${location.origin}/ards-client/docs/v2/classes/hentai/konachan`)
+    }
+    if (location.toString() === `${location.origin}/Ards-Client/classes/hentai/neko`) {
+        location.replace(`${location.origin}/ards-client/docs/v2/classes/hentai/neko`)
+    }
+    if (location.toString() === `${location.origin}/Ards-Client/classes/hentai/rule34`) {
+        location.replace(`${location.origin}/ards-client/docs/v2/classes/hentai/rule34`)
+    }
+    if (location.toString() === `${location.origin}/Ards-Client/classes/hentai/yandere`) {
+        location.replace(`${location.origin}/ards-client/docs/v2/classes/hentai/yandere`)
+    }
+    if (location.toString() === `${location.origin}/Ards-Client/classes/meme`) {
+        location.replace(`${location.origin}/ards-client/docs/v2/classes/meme`)
+    }
+    if (location.toString() === `${location.origin}/Ards-Client/classes/meme/random`) {
+        location.replace(`${location.origin}/ards-client/docs/v2/classes/meme/random`)
+    }
+    if (location.toString() === `${location.origin}/Ards-Client/classes/porn`) {
+        location.replace(`${location.origin}/ards-client/docs/v2/classes/porn`)
+    }
+    if (location.toString() === `${location.origin}/Ards-Client/classes/porn/ass`) {
+        location.replace(`${location.origin}/ards-client/docs/v2/classes/porn/ass`)
+    }
+    if (location.toString() === `${location.origin}/Ards-Client/classes/porn/boobs`) {
+        location.replace(`${location.origin}/ards-client/docs/v2/classes/porn/boobs`)
+    }
+    if (location.toString() === `${location.origin}/Ards-Client/classes/porn/panties`) {
+        location.replace(`${location.origin}/ards-client/docs/v2/classes/porn/panties`)
+    }
+    if (location.toString() === `${location.origin}/Ards-Client/classes/porn/pussy`) {
+        location.replace(`${location.origin}/ards-client/docs/v2/classes/porn/pussy`)
+    }
+    if (location.toString() === `${location.origin}/Ards-Client/classes/porn/random`) {
+        location.replace(`${location.origin}/ards-client/docs/v2/classes/porn/random`)
+    }
+    if (location.toString() === `${location.origin}/Ards-Client/classes/porn/thighs`) {
+        location.replace(`${location.origin}/ards-client/docs/v2/classes/porn/thighs`)
+    }
+    if (location.toString() === `${location.origin}/Ards-Client/classes/reddit`) {
+        location.replace(`${location.origin}/ards-client/docs/v2/classes/reddit`)
+    }
+    if (location.toString() === `${location.origin}/Ards-Client/classes/reddit/custom`) {
+        location.replace(`${location.origin}/ards-client/docs/v2/classes/reddit/custom`)
+    }
+    if (location.toString() === `${location.origin}/Ards-Client/classes/reddit/custom_1`) {
+        location.replace(`${location.origin}/ards-client/docs/v2/classes/reddit/custom_1`)
+    }
 }
