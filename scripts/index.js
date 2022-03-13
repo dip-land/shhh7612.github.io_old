65a97b24bcc065c1cc18f55a83c6ca553df65140
shhh7612 <82669765+shhh7612@users.noreply.github.com>
2022-03-13 06:08:04 +0000

    Deploying to gh-pages from @ shhh7612/shhh7612.github.io@58b98e025bcfee3e56e14ec868727cf9174a1901 🚀

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,14 @@
 "use strict";
+if (JSON.parse(localStorage.getItem('settings') || '{}').darkmode === "false") {
+    toggleDM();
+}
+function toggleDM() {
+    document.documentElement.classList.toggle('default');
+    document.documentElement.classList.toggle('dark_mode');
+    if (document.documentElement.classList.contains('default')) {
+        localStorage.setItem('settings', '{"darkmode": "false"}');
+    }
+    else {
+        localStorage.setItem('settings', '{"darkmode": "true"}');
+    }
+}
